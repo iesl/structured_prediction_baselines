880d720ad5bce8badf158977cb74b1c2fa752c05
Purujit Goyal <puru072@gmail.com>
2021-08-03 01:14:30 -0400

    adds multi-label-score-loss from dev/jy

--- structured_prediction_baselines/modules/loss/multilabel_classification/dvn_score_loss.py
+++ structured_prediction_baselines/modules/loss/multilabel_classification/dvn_score_loss.py
@@ -16,7 +16,25 @@ class MultiLabelDVNScoreLoss(DVNScoreLoss):
         predicted_score: torch.Tensor,  # logits of shape (batch, num_samples)
     ) -> torch.Tensor:
         return -torch.sigmoid(predicted_score)
-      
+
+
+@Loss.register("multi-label-score-loss")
+class MultiLabelScoreLoss(DVNScoreLoss):
+    """
+    Non-DVN setup where score is not bounded in [0,1],
+    however the only thing we need is score from scoreNN,
+    so it's better to share with DVNScoreLoss.
+    """
+
+    def normalize(self, y: torch.Tensor) -> torch.Tensor:
+        return _normalize(y)
+
+    def compute_loss(
+        self,
+        predicted_score: torch.Tensor,  # logits of shape (batch, num_samples)
+    ) -> torch.Tensor:
+        return -predicted_score
+
 
 @Loss.register("multi-label-dvn-ca-score")
 class MultiLabelDVNScoreCA(DVNScoreCostAugNet):
