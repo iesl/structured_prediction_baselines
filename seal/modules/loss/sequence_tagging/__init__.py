1b44f4f52810773392ff71f3b2cf842bc8120000
dhruvdcoder <dhruveshpate@umass.edu>
2021-10-22 15:04:25 -0400

    Remove tagging related model components

--- seal/modules/loss/sequence_tagging/__init__.py
+++
@@ -1,5 +0,0 @@
-from .cross_entropy import SequenceTaggingMaskedCrossEntropyWithLogitsLoss
-from .inference_net_loss import (
-    SequenceTaggingMarginBasedLoss,
-    SequenceTaggingInferenceLoss,
-)
