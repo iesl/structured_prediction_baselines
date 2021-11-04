Fix attention_mask calculation ins self-attention structured score, adds self-attention-full-sequence as separate type of structured score

structured_prediction_baselines/modules/structured_score/sequence_tagging/self_attention.py
@@ -15,6 +15,7 @@ class SelfAttention(StructuredScore):
         self.num_tags = num_tags
         self.reduction = reduction
         self.M = M
+        assert self.M >= 0
         # self.seq_length = seq_length
         self.attention_layer = SelfAttentionEncoder(1, num_tags, num_tags, num_tags)
 
@@ -26,12 +27,7 @@ class SelfAttention(StructuredScore):
     ) -> torch.Tensor:
         mask = buffer["mask"]
         batch_size, n_samples, seq_length, _ = y.shape
-        device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        attention_mask = torch.BoolTensor(seq_length, seq_length).fill_(False)
-        attention_mask = attention_mask.to(device=device)
-        for i in range(seq_length):
-            lower_idx, higher_idx = max(0, i - self.M), min(seq_length, i + self.M + 1)
-            attention_mask[i][lower_idx:higher_idx] = True
+        attention_mask = self._get_attention_mask(batch_size, n_samples, seq_length, mask)
 
         attention_output = self.attention_layer(
             y.view(batch_size * n_samples, seq_length, -1),
@@ -47,3 +43,32 @@ class SelfAttention(StructuredScore):
 
         # reduction = "max" (Default)
         return attention_output.amax(dim=3).sum(2)
+
+    def _get_attention_mask(self, batch_size, n_samples, seq_length, mask):
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        attention_mask = torch.BoolTensor(batch_size * n_samples, seq_length, seq_length).fill_(False)
+        attention_mask = attention_mask.to(device=device)
+        masked_length = mask.sum(dim=-1)
+
+        for i in range(batch_size):
+            i_seq_len = masked_length[i]
+            for j in range(i_seq_len):
+                lower_idx, higher_idx = max(0, j - self.M), min(i_seq_len, j + self.M + 1)
+                attention_mask[i:i + n_samples, j, lower_idx:higher_idx] = True
+
+        return attention_mask
+
+
+@StructuredScore.register("self-attention-full-sequence")
+class SelfAttentionFullSequence(SelfAttention):
+    def __init__(self, num_tags: int, reduction: str = "max", **kwargs: Any):
+        """
+        TODO: Change kwargs to take hidden size and output size
+        """
+        super().__init__(num_tags, reduction, **kwargs)
+
+    def _get_attention_mask(self, batch_size: int, n_samples: int, seq_length: int, mask: torch.Tensor):
+        attention_mask = mask.unsqueeze(1).expand(-1, seq_length, seq_length)
+        attention_mask = attention_mask.repeat(1, n_samples, 1).view(batch_size * n_samples, seq_length, seq_length)
+        return attention_mask
+