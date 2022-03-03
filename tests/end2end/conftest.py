76e28baa5e2faf76f41a9f25788e6aef83deb313
Dhruv Agarwal <dhruv.agarwal17@gmail.com>
2022-03-03 19:35:15 +0530

    Added fix for NLTK LookupError

--- tests/end2end/conftest.py
+++ tests/end2end/conftest.py
@@ -1,5 +1,11 @@
 from .common import marks
+import nltk
 
+
+try:
+    nltk.data.find('corpora/omw-1.4')
+except LookupError:
+    nltk.download('omw-1.4')
 
 def pytest_configure(config) -> None:  # type: ignore
     for mark in marks:
